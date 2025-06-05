bd9b861a9c62e8c8fd98a29082d6340074705fe3
Yokesh-Manimannan04 <m.yokesh05@gmail.com>
2025-06-05 14:54:00 +0530

    Add files via upload

---
+++ task24.java
@@ -0,0 +1,23 @@
+class Student{
+    public String name;
+    Student(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
+
+public class task24{
+    public static void main (String[] args){
+        Student[] myStudents = new Student[]{
+                new Student("Dharma"),new Student("sanvi"),
+                new Student("Rupa"),new Student("Ajay")
+        };
+        for(Student m:myStudents){
+            System.out.println(m);
+        }
+    }
+}
---
+++ task26.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class GFG {
+    public static void main(String[] args){
+        int[][] arr = new int[3][3];
+        System.out.println("Rows : " + arr.length);
+        System.out.println("Columns : " + arr[0].length);
+    }
+}
